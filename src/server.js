20915cf9d6275db04bdcb7f5fa9ecbf324038ac4
Anna Havelia <annahavelia@Annas-Laptop.local>
2025-02-14 17:57:06 +0200

    Updated root file

--- src/server.js
+++ src/server.js
@@ -7,34 +7,36 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const getUser = async (token) => {
-  if (token) {
-    try {
-      console.log("SECRET_KEY", process.env.SECRET_KEY);
-      const decoded = jwt.verify(
-        token.replace("Bearer ", ""),
-        process.env.SECRET_KEY
-      );
-      console.log("decoded", decoded);
-      return await User.findByPk(decoded.userId);
-    } catch (err) {
-      console.error("Invalid token", err);
-      return null;
+export const startServer = async () => {
+  const getUser = async (token) => {
+    if (token) {
+      try {
+        console.log("SECRET_KEY", process.env.SECRET_KEY);
+        const decoded = jwt.verify(
+          token.replace("Bearer ", ""),
+          process.env.SECRET_KEY
+        );
+        console.log("decoded", decoded);
+        return await User.findByPk(decoded.userId);
+      } catch (err) {
+        console.error("Invalid token", err);
+        return null;
+      }
     }
-  }
-  return null;
+    return null;
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      const token = req.headers.authorization || "";
+      const user = await getUser(token);
+      return { user };
+    },
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
 };
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const token = req.headers.authorization || "";
-    const user = await getUser(token);
-    return { user };
-  },
-});
-
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
