30a8067f7a0c9ae48bb34b971970466fee927d31
Anna Havelia <annahavelia@Annas-Laptop.local>
2025-02-18 16:01:28 +0200

    Added cors policy

--- src/server.js
+++ src/server.js
@@ -7,25 +7,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const startServer = async () => {
-  const getUser = async (token) => {
-    if (token) {
-      try {
-        console.log("SECRET_KEY", process.env.SECRET_KEY);
-        const decoded = jwt.verify(
-          token.replace("Bearer ", ""),
-          process.env.SECRET_KEY
-        );
-        console.log("decoded", decoded);
-        return await User.findByPk(decoded.userId);
-      } catch (err) {
-        console.error("Invalid token", err);
-        return null;
-      }
+const getUser = async (token) => {
+  if (token) {
+    try {
+      const decoded = jwt.verify(
+        token.replace("Bearer ", ""),
+        process.env.SECRET_KEY
+      );
+      return await User.findByPk(decoded.userId);
+    } catch (err) {
+      console.error("Invalid token", err);
+      return null;
     }
-    return null;
-  };
+  }
+  return null;
+};
 
+export const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -33,6 +31,10 @@ export const startServer = async () => {
       const token = req.headers.authorization || "";
       const user = await getUser(token);
       return { user };
+    },
+    cors: {
+      origin: [process.env.DEV_CLIENT_URL, process.env.PROD_CLIENT_URL],
+      credentials: true,
     },
   });
 
